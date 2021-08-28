f5f88183be46519597197591d3684da83d6a8109
polklabs <polkandrew01@gmail.com>
2021-08-28 12:22:51 -0700

    Add project files.

---
+++ Tracery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tracery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
