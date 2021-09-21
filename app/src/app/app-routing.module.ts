96f00ebf8f54a60fc4258b623ad8aa89508ab4da
Andrew Polk <polkandrew01@gmail.com>
2021-09-20 19:03:00 -0700

    Initial survey-taker code

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [
     {
         path: 'survey/:id',
         component: SurveyTakerComponent,
+        canDeactivate: [CanDeactivateGuard],
         data: {
             title: 'Survey OTM',
         }
--- app/src/app/survey-taker/survey-taker.component.ts
+++ app/src/app/survey-taker/survey-taker.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataService } from '../core/services/data.service';
+import { DialogService } from '../core/services/dialog.service';
+import { Survey } from '../shared/model/survey.model';
 
 @Component({
-  selector: 'app-survey-taker',
-  templateUrl: './survey-taker.component.html',
-  styleUrls: ['./survey-taker.component.scss']
+    selector: 'app-survey-taker',
+    templateUrl: './survey-taker.component.html',
+    styleUrls: ['./survey-taker.component.scss']
 })
 export class SurveyTakerComponent implements OnInit {
 
-  constructor() { }
+    id = '';
+    survey: Survey = new Survey();
 
-  ngOnInit(): void {
-  }
+    debounceButton = false;
+    loading: boolean[] = [];
+    loadingUnknown = false;
+
+    dirty = false;
+
+    constructor(
+        private dialogService: DialogService,
+        private activatedRoute: ActivatedRoute,
+        private dataService: DataService
+    ) { }
+
+    ngOnInit(): void {
+        this.activatedRoute.paramMap.subscribe(params => { 
+            const id = params.get('id');
+            if (id && id !== '0') {
+                this.id = id;
+                this.getSurvey();
+            } else {
+                window.alert('No Survey!');
+            }
+        });
+    }
+
+    canDeactivate(): Observable<boolean> | boolean {
+        if (this.dirty) {
+            return this.dialogService.confirm('Discard changes for Survey?');
+        }
+        return true;
+    }
+
+    getSurvey(): void {
+        this.loadingUnknown = true;
+        const [result, _] = this.dataService.getData(`survey?id=${this.id}`);
+        result.subscribe((data: { ok: boolean, data?: Survey, headers?: any, status?: any, error?: any }) => {
+            if (data.ok) {
+                this.survey = data.data!;
+            } else {
+                this.dialogService.alert(`Error: ${data.error}`);
+            }
+            this.loadingUnknown = false;
+        });
+    }
 
 }
