fa2bbdff80be9308cd2bf8e9a1fb5046ed63bcf0
Andrew Polk <polkandrew01@gmail.com>
2021-09-21 19:33:30 -0700

    Improved handling of couch-db documents

--- src/couch.ts
+++ src/couch.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import crypto from 'crypto';
 import { Survey } from '../app/src/app/shared/model/survey.model';
 import { SurveyContainer } from '../app/src/app/shared/model/survey-container.model';
-import { Answer } from '../app/src/app/shared/model/answer.model';
+import { Answer, AnswerStatus } from '../app/src/app/shared/model/answer.model';
 import { response } from '../server';
 
 // TODO: Make into await/synchronous functions
@@ -32,6 +32,31 @@ export function upsertSurvey(survey: Survey, id: string, key: string, res: respo
 function updateSurvey(container: SurveyContainer, survey: Survey, res: response): void {
     container.survey = survey;
     container.lastModifiedDate = new Date().toISOString();
+    container.survey.lastModifiedDate = new Date().toISOString();
+
+    // Delete non existent users and non existent questions
+    const toDelete_user: number[] = [];
+    container.answers.forEach((user, user_index) => {
+        if (container.survey.users.findIndex(x => x._id === user.userId) === -1) {
+            toDelete_user.push(user_index);
+        } else {
+            const toDelete_answer: number[] = [];
+            user.answers.forEach((answer, answer_index) => {
+                if (container.survey.questions.findIndex(x => x.questionId === answer.questionId) === -1) {
+                    toDelete_answer.push(answer_index);
+                }
+            });
+            toDelete_answer.reverse();
+            toDelete_answer.forEach(index => {
+                user.answers.splice(index, 1);
+            });
+        }
+    })
+    toDelete_user.reverse();
+    toDelete_user.forEach(index => {
+        container.answers.splice(index, 1);
+    });
+
     couch.update('survey', container).then(({ data }) => {
         res.json({ ...data, key: container.key });
     }, (error: any) => {
@@ -91,22 +116,44 @@ export function deleteSurvey(id: string, key: string, res: response): void {
 
 export function answerStatus(id: string, res: response): void {
     couch.get('survey', id).then(({ data }: { data: SurveyContainer }) => {
-        const answerStatus = data.answers.map(x => { return { id: x.userId, count: x.answers.length } });
+        const answerStatus: AnswerStatus[] = [];
+        data.survey.users.forEach(u => {
+            const a = data.answers.find(x => x.userId === u._id);
+            answerStatus.push({
+                userId: u._id, 
+                name: u.name, 
+                count: a?.answers.length??0, 
+                lastModifiedDate: a?.lastModifiedDate, 
+                answered: a?.answers.map(x => {return {questionId: x.questionId, lastModifiedDate: x.lastModifiedDate}})??[] 
+            });
+        });
         res.json({ ok: true, data: answerStatus });
     }, (error: any) => {
         res.json({ ok: false, error });
     });
 }
 
-export function submitAnswers(id: string, answers: Answer, res: response): void {
+// Submit answers ----------------------------------------------------------------------
+
+export function submitAnswers(id: string, answer: Answer, res: response): void {
     couch.get('survey', id).then(({ data }: { data: SurveyContainer }) => {
 
-        const index = data.answers.findIndex(x => x.userId === answers.userId);
+        data.lastModifiedDate = new Date().toISOString();
+
+        const index = data.answers.findIndex(x => x.userId === answer.userId);
         if (index === -1) {
-            data.answers.push(answers);
+            data.answers.push(answer);
         } else {
-            answers.createdDate = data.answers[index].createdDate;
-            data.answers[index] = answers;
+            data.answers[index].lastModifiedDate = answer.lastModifiedDate;
+
+            answer.answers.forEach(a => {
+                const i = data.answers[index].answers.findIndex(x => x.questionId === a.questionId);
+                if (i === -1) {
+                    data.answers[index].answers.push(a);    
+                } else {
+                    data.answers[index].answers[i] = a;
+                }
+            });
         }
 
         couch.update('survey', data).then(({ data }) => {
